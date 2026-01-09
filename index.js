2dd498d41d838e2876e027a1cccebe15fc12d76c
coder-Eno-01 <enovicklupitshi@gmail.com>
2026-01-09 23:26:52 +0200

    Add initial HTML structure for My Projects page

---
+++ index.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Projects</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+    <h1>My Projects</h1>
+    <p>A collection of programming projects.</p>
+
+    <ul>
+        <li>
+            <a href="./2048%20Game/">2048 Game</a>
+        </li>
+    </ul>
+
+</body>
+</html>
