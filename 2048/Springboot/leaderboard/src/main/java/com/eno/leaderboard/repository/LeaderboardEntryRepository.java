c278519dab8d202e1425204110b824a7c4a0c81a
coder-Eno-01 <enovicklupitshi@gmail.com>
2026-01-13 01:48:52 +0200

    Fixed Java Runtime error from missing repository method header definition

--- 2048/Springboot/leaderboard/src/main/java/com/eno/leaderboard/repository/LeaderboardEntryRepository.java
+++ 2048/Springboot/leaderboard/src/main/java/com/eno/leaderboard/repository/LeaderboardEntryRepository.java
@@ -4,9 +4,12 @@ import com.eno.leaderboard.model.LeaderboardEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LeaderboardEntryRepository
         extends JpaRepository<LeaderboardEntry, Long> {
+
+    Optional<LeaderboardEntry> findByClientUid(String clientUid);
 
     List<LeaderboardEntry> findTop5ByClientUidNotNullOrderByScoreDescCreatedAtAsc();
 }
