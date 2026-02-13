d8922407e914695a38fcf0f44b7ca5b579f7945b
coder-Eno-01 <195686447+coder-Eno-01@users.noreply.github.com>
2026-02-13 15:31:49 +0200

    Corrected the variable name mismatch

--- weather-app/src/main.js
+++ weather-app/src/main.js
@@ -1,4 +1,4 @@
-const   BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL,
+const   BACKEND_BASE_URL = import.meta.env.BACKEND_BASE_URL,
         cityInput = document.querySelector(".weatherData input"),
         cityName = document.getElementById("cityName"),
         temperature = document.getElementById("temp"),
