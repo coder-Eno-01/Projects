b86ce6ec3f71282d588006d6c647d4c41528f97b
coder-Eno-01 <195686447+coder-Eno-01@users.noreply.github.com>
2026-04-30 21:14:53 +0200

    Updated Health Controller to touch DB

--- 2048/Springboot/leaderboard/src/main/java/com/eno/leaderboard/controller/HealthController.java
+++ 2048/Springboot/leaderboard/src/main/java/com/eno/leaderboard/controller/HealthController.java
@@ -1,13 +1,26 @@
 package com.eno.leaderboard.controller;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 public class HealthController {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public HealthController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @GetMapping("/health")
-    public String health() {
-        return "OK";
+    public Map<String, String> health() {
+        jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+
+        return Map.of(
+            "server", "UP",
+            "database", "UP"
+        );
     }
 }
